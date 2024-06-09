7a53e9032967f38a389d5a964e25acbb4c0a9679
joacs0 <138056664+joacs0@users.noreply.github.com>
2024-06-09 19:49:42 -0400

    C++

---
+++ Operadores.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+int main(){
+
+    int hormigas = 20;
+
+    // int estudiantes = 20;
+    
+    // Suma
+    
+    // estudiantes = estudiantes + 2;
+    // estudiantes += 2;
+    // estudiantes++;
+
+    //Resta
+
+    // estudiantes = estudiantes + 2;
+    // estudiantes -= 2;
+    // estudiantes--;
+
+    // Multiplicacion
+
+    // estudiantes = estudiantes * 2;
+    // estudiantes *= 2;
+    
+
+    // Division
+
+    // estudiantes = estudiantes / 3;
+    // estudiantes /= 3;
+    
+    // Modulo (%)
+
+    // int resto = estudiantes % 3;
+
+    // hormigas = hormigas + 2;
+
+    // std::cout<<hormigas<<std::endl;
+    
+    
+     // hormigas = 2 + 3 - 2 * 7; =
+     hormigas = 10 + (3 - 2) * 7;
+
+     std::cout<<hormigas<<std::endl;
+
+
+
+
+    return 0;
+}
---
+++ typedef.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+// typedef std::string text_t;
+// typedef int numero_t;
+
+using Nombre = std::string;
+using Edad = int;
+
+int main(){
+
+     Nombre Nombre = "Joa";
+     Edad Edad = 15;
+
+     std::cout<<Nombre<<" "<<Edad<<std::endl;
+}
